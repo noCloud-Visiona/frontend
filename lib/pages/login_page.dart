7dd46bfe41d663ffd4d13c644e22fc8dccc497f0
Thiago Zani <thiago.zani@uol.com.br>
2024-09-26 09:59:50 -0300

    WIR - ajustes login, register e custom_dialog - transformando em widget para reaproveitar em outras paginas

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -22,58 +22,101 @@ class _LoginPageState extends State<LoginPage> {
   Future<void> _login() async {
     // Verificando se os campos estão preenchidos
     if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
-      showCustomDialog(context, "Por favor, preencha todos os campos.", null);
+      _showErrorDialog("Por favor, preencha todos os campos.");
       return;
     }
 
     // Validando o e-mail com regex
     final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+$');
     if (!emailRegex.hasMatch(_emailController.text)) {
-      showCustomDialog(context, "E-mail inválido.", null);
+      _showErrorDialog("E-mail inválido.");
       return;
     }
 
-    final String apiUrl = '${dotenv.env['API_URL']}/login';
-
-    final Map<String, dynamic> loginData = {
-      "email": _emailController.text,
-      "senha": _passwordController.text,
-    };
-
+    // Lógica de login
     try {
       final response = await http.post(
-        Uri.parse(apiUrl),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: jsonEncode(loginData),
+        Uri.parse('${dotenv.env['API_URL']}/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'email': _emailController.text,
+          'senha': _passwordController.text, // Certifique-se de que o campo é 'senha' e não 'password'
+        }),
       );
 
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
       if (response.statusCode == 200) {
-        final responseBody = jsonDecode(response.body);
-        String token = responseBody['token']['token'];
+        final data = jsonDecode(response.body);
+        if (data.containsKey('token') && data['token'].containsKey('token')) {
+          final token = data['token']['token'];
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setString('token', token);
 
-        // Armazenando o token no SharedPreferences
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setString('jwt_token', token);
-
-        showCustomDialog(context, "Login realizado com sucesso!", () {
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(builder: (context) => const HomePage()),
           );
-        });
-      } else if (response.statusCode == 400) {
-        final responseBody = jsonDecode(response.body);
-        showCustomDialog(context, responseBody['error'], null);
+        } else {
+          _showErrorDialog("Resposta inesperada do servidor.");
+        }
       } else {
-        showCustomDialog(
-            context, "Erro ao fazer login. Tente novamente.", null);
+        final responseBody = jsonDecode(response.body);
+        _showErrorDialog(responseBody['mensagem'] ?? "Falha no login. Verifique suas credenciais.");
       }
     } catch (e) {
       print("Erro: $e");
-      showCustomDialog(context, "Erro ao se conectar ao servidor.", null);
+      _showErrorDialog("Erro ao conectar ao servidor. Tente novamente mais tarde.");
     }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomDialog(
+          title: 'Erro',
+          content: Text(
+            message,
+            style: const TextStyle(
+              fontFamily: 'Roboto',
+              shadows: [
+                Shadow(
+                  color: Colors.black54,
+                  offset: Offset(2, 2),
+                  blurRadius: 3,
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: const Color(0xFF009D42),
+              ),
+              child: const Text(
+                "Ok",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Roboto',
+                  shadows: [
+                    Shadow(
+                      color: Colors.black54,
+                      offset: Offset(2, 2),
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
