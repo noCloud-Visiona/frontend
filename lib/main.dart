b27fffdcfd187156ac4fca86c725f3a5f1754659
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2024-09-29 17:07:58 -0300

    fix: adicionando rota do /logout para logout voltar a funcionar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/pages/analisar_img_page.dart';
+import 'package:frontend/pages/history_page.dart';
 import 'package:frontend/pages/login_page.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/providers/auth_provider.dart';
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()), // Adicione seu AuthProvider aqui
+        ChangeNotifierProvider(
+            create: (_) => AuthProvider()), // Adicione seu AuthProvider aqui
       ],
       child: MaterialApp(
         title: 'noCloud',
@@ -32,11 +34,14 @@ class MyApp extends StatelessWidget {
         home: const LoginPage(),
         routes: {
           '/analisar': (context) {
-            final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+            final args = ModalRoute.of(context)!.settings.arguments
+                as Map<String, dynamic>;
             return AnalisarImgPage(
               imgPath: args['imgPath'],
             );
           },
+          '/login': (context) => const LoginPage(),
+          '/lista': (context) => HistoricoPage(),
         },
       ),
     );
