4c496e3a2322b082b0678f3e2523b22cee2e39c0
Thiago Zani <thiago.zani@uol.com.br>
2024-10-07 13:38:20 -0300

    WIR - adicionado mapa na home page do projeto (Flutter_Map)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/template/app_template.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  Future<String> _getUserId() async {
-    final prefs = await SharedPreferences.getInstance();
-    final userId = prefs.getString('userId');
-    if (userId == null) {
-      throw Exception('userId não encontrado em SharedPreferences');
-    }
-    return userId;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<String>(
-      future: _getUserId(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Erro: ${snapshot.error}'));
-        } else {
-          final userId = snapshot.data!;
-          return AppTemplate(
-            currentIndex: 0,
-            body: Center(
-              child: Text('Bem-vindo, usuário $userId'),
-            ),
-          );
-        }
-      },
+    return AppTemplate(
+      currentIndex: 0,
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(-23.5505, -46.6333), // Exemplo: São Paulo
+          maxZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+        ],
+      ),
     );
   }
 }
